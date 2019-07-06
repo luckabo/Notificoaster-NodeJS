97c8eb91b5824382292989eae77583517d9a686f
shetland <shetland@sfu.ca>
2019-07-05 18:18:10 -0700

    more heroku fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Device = require('./models/device')
 const Reading = require('./models/reading')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.port || '0.0.0.0'
 
 app.use(express.json())
 
