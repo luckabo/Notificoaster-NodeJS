a7b143dcf9971a87ce043eea7c0b1e5cd7a9a512
shetland <shetland@sfu.ca>
2019-08-28 14:00:05 -0700

    Removed the device files, just leaves user and reading files

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const readingRouter = require('./routers/reading')
-const deviceRouter = require('./routers/device')
 
 const app = express()
 app.use(express.json())
 app.use(userRouter)
 app.use(readingRouter)
-app.use(deviceRouter)
 
 // app.listen(process.env.PORT, '0.0.0.0')
 const port = process.env.PORT || 3000
